97d6eb0db49f3928687e64becf0234cb078f49ac
dy <dyu32345@gmail.com>
2023-09-26 10:42:26 +0800

    登陆界面精简

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,28 +1,29 @@
 import { GithubOutlined } from '@ant-design/icons';
 import { DefaultFooter } from '@ant-design/pro-components';
+import {PLANET_LINK} from "@/constants";
 const Footer: React.FC = () => {
-  const defaultMessage = '蚂蚁集团体验技术部出品';
+  const defaultMessage = 'dy 前端路上的第一关';
   const currentYear = new Date().getFullYear();
   return (
     <DefaultFooter
       copyright={`${currentYear} ${defaultMessage}`}
       links={[
         {
-          key: 'Ant Design Pro',
-          title: 'Ant Design Pro',
-          href: 'https://pro.ant.design',
+          key: 'Home',
+          title: '档案馆',
+          href: PLANET_LINK,
           blankTarget: true,
         },
         {
           key: 'github',
           title: <GithubOutlined />,
-          href: 'https://github.com/ant-design/ant-design-pro',
+          href: PLANET_LINK,
           blankTarget: true,
         },
         {
           key: 'Ant Design',
-          title: 'Ant Design',
-          href: 'https://ant.design',
+          title: 'WA-TLE',
+          href: PLANET_LINK,
           blankTarget: true,
         },
       ]}
---
+++ src/constants/index.ts
@@ -0,0 +1,3 @@
+export const SYSTEM_LOGO = "https://media.istockphoto.com/id/1313644269/vector/gold-and-silver-circle-star-logo-template.jpg?s=612x612&w=0&k=20&c=hDqCI9qTkNqNcKa6XS7aBim7xKz8cZbnm80Z_xiU2DI=";
+
+export const PLANET_LINK = "https://github.com/WA-TLE";
